37cff4b419557c245d2ebeb2a7f9b928b97ea20f
QuickTriXXX <isthisreal@whatever.com>
2023-10-18 18:38:23 -0400

    added userscontroller, ef and datacontext

---
+++ API/Program.cs
@@ -0,0 +1,35 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseSqlite(connectionString);
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+//IDE Changes
+//added prefix _ for the private fields in settings, search 'prefix'
+//removed prefix this for the private fields in settings, search 'Use this for ctor'
