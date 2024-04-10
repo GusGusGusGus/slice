0f4b43b46960d59e8f2589f72153dc75b2e8cd22
Speedy Guzz <octaguzz@outlook.com>
2024-04-10 14:22:14 +0100

    added jwt interceptor to inject tokens onto http requests;  added member, photo and user entities; added members service, added member card, added uri to environments files, added global styles and component specific styles; changed route.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, ReplaySubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  baseUrl = 'https://localhost:7052/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
