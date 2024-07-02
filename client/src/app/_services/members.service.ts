b9f387d3bd5adcae1a8caf191a5822113c7a3c92
Speedy Guzz <octaguzz@outlook.com>
2024-07-02 23:48:08 +0100

    added like functionality

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -96,6 +96,16 @@ export class MembersService {
   }
 
 
+  addLike(username: string){
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number){
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResults<Partial<Member[]>>(this.baseUrl + 'likes', params);
+  }
+
   private getPaginatedResults<T>(url, params) {
     const paginatedResults: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
